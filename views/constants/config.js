b9f6c4dc7a7d77592fafb36c4d95fe7a2ee0bafa
psx980626 <375180728@qq.com>
2019-04-12 14:10:35 +0800

    pm2配置

--- views/constants/config.js
+++ views/constants/config.js
@@ -1,1 +1,1 @@
-export const DOMAIN = process.env.NODE_ENV === 'dev' ? 'http://127.0.0.1:3003/' : 'http://129.28.54.202:3002';
+export const DOMAIN = process.env.NODE_ENV === 'dev' ? 'http://127.0.0.1:3001/' : 'http://129.28.54.202:3002';
